adc017b449c9c7a6775c3755203e064e00e9475e
Tristan Hanna <tristan.hanna@colorado.edu>
2020-01-31 15:10:11 -0700

    adding

--- js/commit.js
+++ js/commit.js
@@ -2,12 +2,6 @@ $(document).ready(function () {
     //hides all the information to each section
     $(".quote1").hide();
     
-    $(".quoting1").hover(function () {
-        $("#mambaPic").css("padding", "3%");
-        $("#mambaPic").css("background-color", "white");
-        $("#mambaPic").css("border-radius", "1em");
-    });
-    
     //toggles information based on click
     $(".quoting1").click(function () {
         $(".mamba").toggle();
