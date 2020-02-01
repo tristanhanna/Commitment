590e28631425fad365acc9abaf73a867e9820d7d
Tristan Hanna <tristan.hanna@colorado.edu>
2020-02-01 13:38:43 -0700

    fixing API key

--- js/commit.js
+++ js/commit.js
@@ -19,7 +19,7 @@ function retrieve() {
         q: "kobe bryant's best 100 plays of his career",
         part: 'snippet',
         maxResults: 11,
-        key: "AIzaSyBsObOCPoImQ85Z0blZk0pxIXJlEz6XaiY"
+        key: "N/A"
     }
 
     url.search = new URLSearchParams(params);
