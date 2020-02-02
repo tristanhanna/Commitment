22a660977f6734bb9ec0de1e7db47b7ca5eb3202
Tristan Hanna <tristan.hanna@colorado.edu>
2020-02-02 11:12:15 -0700

    more commits

--- js/commit.js
+++ js/commit.js
@@ -2,19 +2,39 @@ $(document).ready(function () {
     //hides all the information to each section
     $(".quote1").hide();
     
+    
+    
     //toggles information based on click
     $(".quoting1").click(function () {
         $(".mamba").toggle();
         $(".quote1").toggle();
     });
-    
-    
-    
 });
-var texts = ["The most important thing is to try and inspire people so that they can be great in whatever they want to do.", "I'm reflective only in the sense that I learn to move forward. I reflect with a purpose.", "I have self-doubt. I have insecurity. I have fear of failure. I have nights when I show up at the arena and I'm like, 'My back hurts, my feet hurt, my knees hurt. I don't have it. I just want to chill.' We all have self-doubt. You don't deny it, but you also don't capitulate to it. You embrace it.", "I don't want to be the next Michael Jordan, I only want to be Kobe Bryant."]
+
+
+var texts = ["The most important thing is to try and inspire people so that they can be great in whatever they want to do.", "I'm reflective only in the sense that I learn to move forward. I reflect with a purpose.", "I have self-doubt. I have insecurity. I have fear of failure. I have nights when I show up at the arena and I'm like, 'My back hurts, my feet hurt, my knees hurt. I don't have it. I just want to chill.' We all have self-doubt. You don't deny it, but you also don't capitulate to it. You embrace it.", "I don't want to be the next Michael Jordan, I only want to be Kobe Bryant.", "I can't relate to lazy people. We don't speak the same language. I don't understand you. I don't want to understand you."]
 var count = 1;
 function slideShowText() {
-    
+    if(count == 1){
+        $("#text").html(texts[0]);
+        count++;
+    }
+    if(count == 2){
+        $("#text").html(texts[1]);
+        count++;
+    }
+    if(count == 3){
+        $("#text").html(texts[2]);
+        count++;
+    }
+    if(count == 4){
+        $("#text").html(texts[3]);
+        count++;
+    }
+    if(count == 5){
+        $("#text").html(texts[4]);
+        count = 1;
+    }
 }
 
 function retrieve() {
